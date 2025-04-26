6386bde494258df6489cdae20946da87b88b10ea
atul-en <atkr5496@gmail.com>
2025-04-26 17:38:35 +0530

    remote app HMR fix

--- apps/remote/server.js
+++ apps/remote/server.js
@@ -1,50 +1,53 @@
-const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-const webpackConfig = require("./webpack.config");
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('./webpack.config');
 
 const app = express();
 const compiler = webpack(webpackConfig);
 
 const PORT = 3001;
 
-const allowedOrigins = ["http://localhost:3000/"];
-const isDevelopment = process.env.NODE_ENV === "development";
+const allowedOrigins = ['http://localhost:3000/'];
+const isDevelopment = process.env.NODE_ENV === 'development';
 
-app.use("/remoteEntry.js", (req, res, next) => {
-  const referer = req.get("origin") || req.get("referer");
+app.use('/remoteEntry.js', (req, res, next) => {
+	const referer = req.get('origin') || req.get('referer');
 
-  if (allowedOrigins.some((origin) => referer && referer.startsWith(origin))) {
-    return next();
-  }
+	if (allowedOrigins.some((origin) => referer && referer.startsWith(origin))) {
+		return next();
+	}
 
-  res.status(403).send("Forbidden: Referer not allowed");
+	res.status(403).send('Forbidden: Referer not allowed');
 });
 
 if (isDevelopment) {
-  // Webpack middlewares
-  app.use(
-    webpackDevMiddleware(compiler, {
-      publicPath: webpackConfig.output.publicPath,
-      stats: "minimal",
-    })
-  );
-  app.use(webpackHotMiddleware(compiler));
+	// Webpack middlewares
+	app.use(
+		webpackDevMiddleware(compiler, {
+			publicPath: webpackConfig.output.publicPath,
+			stats: 'minimal',
+		}),
+	);
+	app.use(
+		webpackHotMiddleware(compiler),
+	);
 } else {
-  // Static file handling for production
-  app.use(express.static(path.resolve(__dirname, "dist")));
+	// Static file handling for production
+	app.use(express.static(path.resolve(__dirname, 'dist')));
 }
 
 // Handle everything else
 app.get('/', (req, res) => {
-    const filePath = isDevelopment
-      ? path.join(compiler.outputPath, 'index.html')
-      : path.resolve(__dirname, 'dist', 'index.html');
-  
-    res.sendFile(filePath);
-  });
+	const filePath = isDevelopment
+		? path.join(compiler.outputPath, 'index.html')
+		: path.resolve(__dirname, 'dist', 'index.html');
+
+	res.sendFile(filePath);
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Remote app running at http://localhost:${PORT}`);
+	console.log(`🚀 Remote app running at http://localhost:${PORT}`);
 });
--- apps/remote/webpack.dev.js
+++ apps/remote/webpack.dev.js
@@ -1,49 +1,54 @@
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const deps = require("./package.json").dependencies;
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
+const webpack = require('webpack');
 
 const configs = {
-  appName: "remoteApp",
-  appFileName: "remoteEntry.js",
-  PORT: 3001,
-  PUBLIC_PATH: "http://localhost:3001/",
-  REMOTES: ["hostApp@http://localhost:3000/remoteEntry.js"],
+	appName: 'remoteApp',
+	appFileName: 'remoteEntry.js',
+	PORT: 3001,
+	PUBLIC_PATH: 'http://localhost:3001/',
+	REMOTES: ['hostApp@http://localhost:3000/remoteEntry.js'],
 };
 
 let remotes = {};
 let REMOTES = configs.REMOTES;
 for (let i = 0; i < REMOTES.length; i++) {
-  remotes[REMOTES[i].split("@")[0]] = REMOTES[i];
+	remotes[REMOTES[i].split('@')[0]] = REMOTES[i];
 }
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
-  output: {
-    publicPath: configs.PUBLIC_PATH,
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: configs.appName,
-      filename: configs.appFileName,
-      exposes: {
-        "./RemoteComponent": "./src/RemoteComponent",
-      },
-      remotes,
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          eager: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          eager: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new ReactRefreshWebpackPlugin(),
-  ],
+	mode: 'development',
+	devtool: 'cheap-module-source-map',
+	entry: {
+		main: ['webpack-hot-middleware/client', './src/index.tsx'],
+	},
+	output: {
+		publicPath: configs.PUBLIC_PATH,
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: configs.appName,
+			filename: configs.appFileName,
+			exposes: {
+				'./RemoteComponent': './src/RemoteComponent',
+			},
+			remotes,
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					eager: true,
+					requiredVersion: deps.react,
+				},
+				'react-dom': {
+					singleton: true,
+					eager: true,
+					requiredVersion: deps['react-dom'],
+				},
+			},
+		}),
+		new ReactRefreshWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+	],
 };
